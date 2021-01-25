101008f9bad2073a52a90cb66200c163b5880dc7
tacoyaggi93 <tacoyaggi93@gmail.com>
2021-01-25 22:56:52 +0900

    First Git

---
+++ wild/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using wild.Models;
+
+namespace wild.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            return View();
+        }
+    }
+}
---
+++ wild/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wild.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Email을 입력해주세요.")]
+        [MaxLength(10)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "비밀번호를 입력해주세요.")]
+        [MinLength(3)]
+        public string Pass { get; set; }
+    }
+}
